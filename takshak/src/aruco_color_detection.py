f0a62601efc3fb89c66b8d077b3e6c11cddaa0ef
djdheeraj5701 <dheerajgonchigar5701@gmail.com>
2021-09-27 20:54:58 +0530

    abhi toh aayega

--- takshak/src/aruco_color_detection.py
+++ takshak/src/aruco_color_detection.py
@@ -1,21 +1,20 @@
-
 #!/usr/bin/env python
 
-from ros_essentials_cpp.srv import AddTwoInts
-from ros_essentials_cpp.srv import AddTwoIntsRequest
-from ros_essentials_cpp.srv import AddTwoIntsResponse
+from colour_aruco.srv import colour_aruco
+from colour_aruco.srv import colour_arucoRequest
+from colour_aruco.srv import colour_arucoResponse
 
 import rospy
 
 def handle_aruco_color(req):
 	aruco_ids=[]
 	color_ids=[]
-    # store here
+    <>
     return aruco_ids,color_ids
 
 def add_aruco_color_server():
     rospy.init_node('aruco_color_server')
-    s = rospy.Service('aruco_color', AddTwoInts, handle_aruco_color)
+    s = rospy.Service('aruco_color', colour_aruco, handle_aruco_color)
     rospy.spin()
     
 if __name__ == "__main__":
